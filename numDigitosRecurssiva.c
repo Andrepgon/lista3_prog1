697951725626692104b2ee5346697100170f8908
Andrepgon <160280690+Andrepgon@users.noreply.github.com>
2024-07-05 12:56:11 -0300

    Update numDigitosRecurssiva.c

--- numDigitosRecurssiva.c
+++ numDigitosRecurssiva.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int numDigitos(int x)
-{   
-    if (x == 0)
-    {
+int numDigitos(int x) {
+    // Caso base: se x está entre -9 e 9, incluindo 0, tem um dígito
+    if (x < 10 && x > -10) {
         return 1;
     }
-    if (x < 10 && x > -10) 
-    {
-        return 1;
-    }
-    return 1 + numDigitos(x/10); 
+    // Chamada recursiva
+    return 1 + numDigitos(x / 10);
 }
 
+int main() {
+    int numero = 12345;
+    printf("O número %d tem %d dígitos.\n", numero, numDigitos(numero));
+    return 0;
+}
